6f43919b32d82845d86f9865f85e79175904f04b
soheil <jalilisoheil14@gmail.com>
2023-07-12 14:17:37 -0700

    add main and facilities and location

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
-const hamberMenu = document.querySelector(".nav__menu--hamber");
+const hamberMenu = document.querySelector(".nav__menu--hamber"),
+  navBarNav = document.querySelector(".navbar__nav");
+
+let isActive = false;
 
 hamberMenu.addEventListener("click", () => {
-  hamberMenu.classList.toggle("nav__btn--open");
+  if (isActive) {
+    hamberMenu.classList.remove("nav__btn--open");
+    navBarNav.classList.remove("nav__open");
+    isActive = false;
+  } else {
+    hamberMenu.classList.add("nav__btn--open");
+    navBarNav.classList.add("nav__open");
+    isActive = true;
+  }
 });
