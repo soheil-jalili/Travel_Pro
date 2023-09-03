901c157e1e78e31e66cfe2574bf9b4c87233d5ec
soheil <jalilisoheil14@gmail.com>
2023-09-03 02:41:03 -0700

    fix header responsice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
-const hamburgerBtn = document.querySelector(".nav__btn"),
+// Hamburger Menu
+const navBtn = document.querySelector(".nav__btn"),
   navMenu = document.querySelector(".nav-menu");
 
-hamburgerBtn.addEventListener("click", () => {
-  navMenu.classList.add("nav-menu--active");
+let showMenu = false;
+
+const hideNavMenu = () => {
+  navBtn.classList.remove("nav-menu--active");
+  navMenu.style.left = "-25rem";
+  showMenu = false;
+};
+
+navBtn.addEventListener("click", () => {
+  if (showMenu) {
+    hideNavMenu();
+  } else {
+    navBtn.classList.add("nav-menu--active");
+    navMenu.style.left = "0";
+    showMenu = true;
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const isClickInsideMenu =
+    navMenu.contains(event.target) || navBtn.contains(event.target);
+
+  if (!isClickInsideMenu && showMenu) {
+    hideNavMenu();
+  }
 });
